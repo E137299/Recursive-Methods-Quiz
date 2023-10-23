9846eca881e1c46b711aceddbc3bac8633ae717f
David Yin <29613693+E137299@users.noreply.github.com>
2023-10-22 21:00:56 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+
+  }
+
+  // public static boolean isPalindrome(String str)
+
+
+  // public static double power(double x, int n)
+
+  
+}
